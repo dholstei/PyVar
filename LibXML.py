de78ea1918a761811e57a291acb60bea0e2277c1
dholstei <60890386+dholstei@users.noreply.github.com>
2022-11-15 18:43:23 -0600

    Can now extract Python array of nodes from XPath

--- LibXML.py
+++ LibXML.py
@@ -4,15 +4,20 @@ This lib is used instead of the standard Python XML lib because it's using the r
 which means documents and nodes can be shared with C/C++ code/objects directly.
 '''
 
-from asyncio.windows_events import NULL
 import ctypes
 import sys
 from PyQt5.QtWidgets import (
     QApplication, 
     QFileDialog
 )
-from pyparsing import line
-import MessageBox as M
+from numpy import array
+# import MessageBox as M
+import utils
+
+class PointerPtr(ctypes.Structure):
+    _fields_ = [
+        ("ptr",ctypes.c_void_p),        #    pointer
+    ]
 
 class xmlDoc(ctypes.Structure):
     _fields_ = [
@@ -45,7 +50,7 @@ class xmlDoc(ctypes.Structure):
 class xmlNode(ctypes.Structure):
     _fields_ = [
         ("_private",ctypes.c_void_p),   #    application data
-        ("type",ctypes.c_uint16),       #    type number, must be second !
+        ("type",ctypes.c_int16),        #    type number, must be second !
         ("name",ctypes.c_char_p),       #    the name of the node, or the entity
         ("children",ctypes.c_void_p),   #    parent->childs link
         ("last",ctypes.c_void_p),       #    last child link
@@ -79,8 +84,49 @@ class xmlError(ctypes.Structure):
         ("node",ctypes.c_void_p),       #    the node in the tree
     ]
 
-libXmlPath = "C:\\Users\\dholstein\\Documents\\XML\\lib\\libxml2.dll"
+class xmlXPathParserContext(ctypes.Structure):
+    _fields_ = [
+        ("cur",ctypes.c_char_p),        #    the current char being parsed
+        ("base",ctypes.c_void_p),       #    the full expression
+        ("error",ctypes.c_int),         #    error code
+        ("context",ctypes.c_char_p),    #    the evaluation context
+        ("value",ctypes.c_char_p),      #    the current value
+        ("valueNr",ctypes.c_int),       #    number of values stacked
+        ("valueMax",ctypes.c_int),      #    max number of values stacked
+        ("valueTab",ctypes.c_char_p),   #    stack of values
+        ("comp",ctypes.c_char_p),       #    the precompiled expression
+        ("xptr",ctypes.c_int),          #    it this an XPointer expression
+        ("ancestor",ctypes.c_char_p),   #    used for walking preceding axis
+        ("valueFrame",ctypes.c_int),    #    used to limit Pop on the stack
+    ]
+
+class xmlXPathObject(ctypes.Structure):
+    _fields_ = [
+        ("type",ctypes.c_char_p), 
+        ("nodesetval",ctypes.c_void_p), 
+        ("boolval",ctypes.c_int), 
+        ("floatval",ctypes.c_double), 
+        ("stringval",ctypes.c_void_p), 
+        ("user",ctypes.c_void_p), 
+        ("index",ctypes.c_int), 
+        ("user2",ctypes.c_void_p), 
+        ("index2",ctypes.c_int), 
+    ]
+
+class xmlNodeSet(ctypes.Structure):
+    _fields_ = [
+        ("nodeNr",ctypes.c_int),        #    number of nodes in the set
+        ("nodeMax",ctypes.c_int),       #    size of the array as allocated
+        ("nodeTab",ctypes.c_void_p),    #    array of nodes in no particular order @@ with_ns to check whether nam
+    ]
+
+if utils.os == 'Linux':
+    if utils.x64: libXmlPath = "/usr/lib64/libxml2.so.2"
+    else: libXmlPath = "/usr/lib/libxml2.so.2"
+else:
+    libXmlPath = "C:\\Users\\dholstein\\Documents\\XML\\lib\\libxml2.dll"
 libXML = None
+
 class NullDLL(Exception):
     """Exception raised if "libXML = None"
 
@@ -92,7 +138,7 @@ class NullDLL(Exception):
         self.message = message
         super().__init__(self.message)
 
-class NullDocPtr(Exception):
+class xmlNullPtr(Exception):
     """Exception raised if "xmlDocPtr = NULL"
 
     Attributes:
@@ -107,40 +153,38 @@ class LibErr(Exception):
     """Exception raised if xmlError is not NULL
 
     Attributes:
-        message -- explanation of the error
+        err -- XML error structure
     """
 
     def __init__(self, err=None):
         if err == None:
             self.message = "undefined libxml2 error"
         else:
-            self.message = err.contents.file + ":" + line + " msg:" + err.contents.message
+            e = err.contents
+            self.message = e.file.decode("utf-8") + " line:" + str(e.line) + " msg:" + e.message.decode("utf-8")
         super().__init__(self.message)
 
-def xmlGetLastError ():
+def xmlGetLastError () -> xmlError:
 # Get the last global error registered. This is per thread if compiled with thread support.
 # Returns:	NULL if no error occurred or a pointer to the error
 
     global libXML
 # Check if DLL into memory. 
-    if libXML == None:
-        raise NullDLL()
+    if libXML == None: raise NullDLL()
 
 # set up prototype
     libXML.xmlGetLastError.restype = ctypes.POINTER(xmlError) # correct return type
 
 # wrapping in c_char_p or c_int32 isn't required because .argtypes are known.
     ErrPtr = libXML.xmlGetLastError()
-    if ErrPtr.contents == NULL:
-        return None
-    else:
-        return ErrPtr
+    if ErrPtr: return ErrPtr
+    else: return None
 
-def SetLibXmlPath(path):
+def SetLibXmlPath(path: str):
     global libXmlPath
     libXmlPath = path
 
-def xmlReadFile (filename, encoding, options):
+def xmlReadFile (filename: str, encoding: str, options) -> xmlDoc:
 # parse an XML file from the filesystem or the network.
 #         filename:	a file or URL
 #         encoding:	the document encoding, or NULL
@@ -151,7 +195,8 @@ def xmlReadFile (filename, encoding, options):
 # Load DLL into memory. 
     if libXML == None:
         global libXmlPath
-        libXML = ctypes.WinDLL (libXmlPath)
+        if utils.os == 'Linux': libXML = ctypes.CDLL (libXmlPath)
+        else: libXML = ctypes.WinDLL (libXmlPath)
 
 # set up prototype
     libXML.xmlReadFile.restype = ctypes.POINTER(xmlDoc) # correct return type
@@ -160,19 +205,57 @@ def xmlReadFile (filename, encoding, options):
 # wrapping in c_char_p or c_int32 isn't required because .argtypes are known.
     pDoc = libXML.xmlReadFile(filename.encode(), encoding.encode(), options)
     Err = xmlGetLastError()
+    if Err: raise LibErr(Err)
     return pDoc # return xmlDocPtr (xmlDoc*). Add ".contents" in calling program to dereference.
 
-def xmlDocGetRootElement (doc):
+def xmlReadMemory (XML: str, URL: str, encoding: str, options) -> xmlDoc:
+# parse an XML in-memory document and build a tree.
+#     buffer:   a pointer to a char array
+#     size:	    the size of the array
+#     URL:	    the base URL to use for the document
+#     encoding:	the document encoding, or NULL
+#     options:	a combination of xmlParserOption
+#     Returns:	the resulting document tree
+    
+    global libXML
+# Load DLL into memory. 
+    if libXML == None:
+        global libXmlPath
+        if utils.os == 'Linux': libXML = ctypes.CDLL (libXmlPath)
+        else: libXML = ctypes.WinDLL (libXmlPath)
+
+# set up prototype
+    libXML.xmlReadMemory.restype = ctypes.POINTER(xmlDoc) # correct return type
+    libXML.xmlReadMemory.argtypes = ctypes.c_char_p, ctypes.c_int32, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int32
+  
+# wrapping in c_char_p or c_int32 isn't required because .argtypes are known.
+    pDoc = libXML.xmlReadMemory(XML.encode(), len(XML), URL.encode(), encoding.encode(), options)
+    Err = xmlGetLastError()
+    if Err: raise LibErr(Err)
+    return pDoc # return xmlDocPtr (xmlDoc*). Add ".contents" in calling program to dereference.
+
+def xmlFreeDoc (cur: ctypes.c_void_p) :
+# Free up all the structures used by a document, tree included.
+#     cur:	pointer to the document
+
+# set up prototype
+    libXML.xmlFreeDoc.argtypes = ctypes.c_void_p,
+  
+# wrapping in c_char_p or c_int32 isn't required because .argtypes are known.
+    libXML.xmlFreeDoc(cur)
+    Err = xmlGetLastError()
+    if Err: raise LibErr(Err)
+    return
+
+def xmlDocGetRootElement (doc: xmlDoc) -> xmlNode:
 # Get the root element of the document (doc->children is a list containing possibly comments, PIs, etc ...).
 #     doc:	the document
 #     Returns:	the #xmlNodePtr for the root or NULL
 
-    if doc == None:
-        raise NullDocPtr()
+    if doc == None: raise xmlNullPtr("XML Document pointer may not be NULL")
     global libXML
 # Check if DLL into memory. 
-    if libXML == None:
-        raise NullDLL()
+    if libXML == None: raise NullDLL()
 
 # set up prototype
     libXML.xmlDocGetRootElement.restype = ctypes.POINTER(xmlNode) # correct return type
@@ -182,12 +265,89 @@ def xmlDocGetRootElement (doc):
     NodePtr = libXML.xmlDocGetRootElement(doc)
     return NodePtr   #   return xmlNodePtr (xmlNode*). Add ".contents" in calling program to dereference.
 
+def xmlXPathNewContext (doc: xmlDoc) -> xmlXPathParserContext:
+# Create a new xmlXPathContext
+#     doc:	the XML document
+#     Returns:	the xmlXPathContext just allocated. The caller will need to free it.
+
+    if doc == None: raise xmlNullPtr("XML Document pointer may not be NULL")
+    global libXML
+
+# Check if DLL loaded. 
+    if libXML == None: raise NullDLL()
+
+# set up prototype
+    libXML.xmlXPathNewContext.restype = ctypes.POINTER(xmlXPathParserContext) # correct return type
+    libXML.xmlXPathNewContext.argtypes = ctypes.c_void_p,
+    return libXML.xmlXPathNewContext(doc)
+    
+def xmlXPathFreeContext (ctxt: xmlXPathParserContext) :
+# Free up an xmlXPathContext
+#     ctxt:	the context to free
+
+# set up prototype
+    libXML.xmlXPathFreeContext.argtypes = ctypes.c_void_p,
+    libXML.xmlXPathFreeContext(ctxt)
+    Err = xmlGetLastError()
+    if Err: raise LibErr(Err)
+    return
+
+def xmlXPathEval (str: str, ctx: xmlXPathParserContext) -> xmlXPathObject:
+# Evaluate the XPath Location Path in the given context.
+#     str:	the XPath expression
+#     ctx:	the XPath context
+#     Returns:	the xmlXPathObjectPtr resulting from the evaluation or NULL. the caller has to free the object.
+
+#   Note: The reason I don't open/close a context ptr for each XPath is you may want to keep it around for multiple queries, or not
+    if ctx == None: raise xmlNullPtr("XPath context pointer may not be NULL")
+    global libXML
+# Check if DLL is loaded. 
+    if libXML == None: raise NullDLL()
+
+# set up prototype
+    libXML.xmlXPathEval.restype = ctypes.POINTER(xmlXPathObject) # correct return type
+    libXML.xmlXPathEval.argtypes = ctypes.c_char_p, ctypes.c_void_p,
+    return libXML.xmlXPathEval(str.encode() , ctx)
+
+def xmlNodeArray(xmlXPathObj: xmlXPathObject) -> array:
+    if not xmlXPathObj: return None # null XPath results
+    x = xmlXPathObj.contents.nodesetval
+    if not x: return None           # no nodeset
+    ns = ctypes.cast(x, ctypes.POINTER(xmlNodeSet)).contents
+    if ns.nodeNr == 0: return None  # empty nodeset
+    n = []
+    xmlNodePtr = ctypes.POINTER(xmlNode)
+    xmlNodePtrPtr = ctypes.POINTER(xmlNodePtr)
+    for i in range(ns.nodeNr):
+        p = ctypes.cast(ns.nodeTab + i*ctypes.sizeof(xmlNodePtr), 
+                            xmlNodePtrPtr)
+        print(p.contents.contents.name)
+        n.append(p.contents.contents)   #   dereference twice since pointer to pointer
+    return n   # return python array of NodeSet 
+
 def test():
     app = QApplication(sys.argv)
     FileObj = QFileDialog.getOpenFileName(None, "Select XML file to load", None, "XML (*.xml)")
 
     if len(FileObj[0]) > 0:
         pDoc = xmlReadFile(FileObj[0], "UTF-8", 0)
+        ctypes.c_double
+        if True:
+            # M.MessageBox(0, f"{pDoc:#0{10}x}", "XML doc ptr", 0)
+            D = pDoc.contents
+            print(D.encoding)
+        pNode = xmlDocGetRootElement(pDoc)
+        root = pNode.contents
+        print (root.name)
+    else:
+        s = """
+ <notice>
+  <to>Tove</to>
+  <from>Jani</from>
+  <heading>Reminder</heading>
+  <body>Don't forget me this weekend!</body>
+</notice>   """
+        pDoc = xmlReadMemory(s, "", "UTF-8", 0)
         
         if True:
             # M.MessageBox(0, f"{pDoc:#0{10}x}", "XML doc ptr", 0)
@@ -196,9 +356,17 @@ def test():
         pNode = xmlDocGetRootElement(pDoc)
         root = pNode.contents
         print (root.name)
+    
+    ctxt = xmlXPathNewContext(pDoc)
+    # XPathObj = xmlXPathEval("count(/*/*)", ctxt)
+    # print (int(XPathObj.contents.floatval))
+    XPathObj = xmlXPathEval("/*/*", ctxt)
+    NodeSet = xmlNodeArray(XPathObj)
+    for n in NodeSet:
+        print(n.name)
+    xmlXPathFreeContext(ctxt)
+    if pDoc : xmlFreeDoc(pDoc)
 
-
-    # M.MessageBox(0, "fin", "Caption-y stuff", 0)
     return
 
 test()
